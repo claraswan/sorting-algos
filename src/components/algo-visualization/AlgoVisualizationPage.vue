f7378785df91562646b6e3e9990d8da96ce9ef72
Clara Swanson <claraswanson671@gmail.com>
2024-04-09 14:22:45 +0200

    Added bblsort unit test and generate numbers function

--- src/components/algo-visualization/AlgoVisualizationPage.vue
+++ src/components/algo-visualization/AlgoVisualizationPage.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import AlgoVisualizationBarChart from '@/components/algo-visualization/AlgoVisualizationBarChart.vue'
 import { Algorithm } from '@/services/types'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import bblSort from '@/algorithms/bubbleSort'
 
 const props = defineProps<{
     algorithm: Algorithm
 }>()
 
-const numbersToSort = ref([2, 3, 1, 8]);
+const numbersToSort = ref([] as Array<number>);
+const numbersToSortLength = ref(10);
 
 const startSort = () => {
     if (props.algorithm === Algorithm.bubble) {
@@ -19,14 +20,36 @@ const startSort = () => {
     }
 }
 
+const generateNumbers = () => {
+    numbersToSort.value = Array.from(
+        { length: numbersToSortLength.value }, 
+        () => Math.floor(Math.random() * numbersToSortLength.value + 1)
+    );
+}
+
+onMounted(() => {
+    generateNumbers();
+})
+
 </script>
 
 <template>
     <h1>{{ algorithm }} sort</h1>
     <div class="algo-visual__wrapper">
         <AlgoVisualizationBarChart :data="numbersToSort" />
-        <button class="btn btn-primary" @click="startSort">Start sort</button>
+        <div class="algo-visual__actions d-flex gap-2 m-4">
+            <button class="btn btn-primary" @click="generateNumbers">Generate new numbers</button>
+            <button class="btn btn-primary" @click="startSort">Start sort</button>
+        </div>
+
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.algo-visual {
+    &__wrapper {
+        max-height: 70vh;
+        width: 100%;
+    }
+}
+</style>
